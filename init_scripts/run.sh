2e0982bfd1671f2e3a4a67a466c02fc1351e54a2
Habibur Rahaman Emon <32496488+Emon46@users.noreply.github.com>
2021-04-06 18:41:34 +0600

    Custom uid (#2)
    Signed-off-by: Emon46 <emon@appscode.com>

--- init_scripts/run.sh
+++ init_scripts/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -euo pipefail
-
 rm -rf /run_scripts/*
 cp /tmp/scripts/* /scripts
+chmod 0777 /var/pv
 if [[ $MAJOR_PG_VERSION -gt 11 ]]; then
     rm -rf /scripts/config_recovery.conf.sh /scripts/do_pg_recovery_cleanup.sh
 fi
@@ -11,5 +11,5 @@ if [ "${SSL:-0}" = "ON" ]; then
     cp -R /certs/ /tls/
     chmod 0600 /tls/certs/server/*
     chmod 0600 /tls/certs/client/*
-    chown -R 70:70 /tls/certs
+    chown -R $DB_UID:$DB_GID /tls/certs
 fi
