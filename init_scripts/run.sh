18d83e3d26629d08cc86e85cf181414f0cd8f35a
Habibur Rahaman Emon <32496488+Emon46@users.noreply.github.com>
2021-06-22 16:34:26 +0600

    Remove chmod for volume mount paths (#5)
    Remove chmod for volume mount path which is used for Debian image
    
    Signed-off-by: Emon46 <emon@appscode.com>

--- init_scripts/run.sh
+++ init_scripts/run.sh
@@ -7,11 +7,7 @@ cp /tmp/scripts/* /scripts
 if [[ -e /var/pv/data/PG_VERSION ]]; then
     chmod 0700 /var/pv/data
 fi
-if [[ "${OLD_SHARE_DIR:-"null"}" != "null" ]]; then
-    # Need to set global permission so that all user have the read write permission. Need this one for upgrade in Ops req
-    chmod 0777 $OLD_SHARE_DIR
-    chmod 0777 $OLD_LIB_DIR
-fi
+
 if [[ $MAJOR_PG_VERSION -gt 11 ]]; then
     rm -rf /scripts/config_recovery.conf.sh /scripts/do_pg_recovery_cleanup.sh
 fi
