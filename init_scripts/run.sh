c25931cc91913558ba84718890f749f090d89591
Habibur Rahaman Emon <32496488+Emon46@users.noreply.github.com>
2021-06-22 14:07:18 +0600

    Custom UID changes (#4)
    Signed-off-by: Emon46 <emon@appscode.com>

--- init_scripts/run.sh
+++ init_scripts/run.sh
@@ -3,8 +3,12 @@
 set -euo pipefail
 rm -rf /run_scripts/*
 cp /tmp/scripts/* /scripts
-chmod 0777 /var/pv
+# need to set the chmod to 0700 for running postgres server in single user mode
+if [[ -e /var/pv/data/PG_VERSION ]]; then
+    chmod 0700 /var/pv/data
+fi
 if [[ "${OLD_SHARE_DIR:-"null"}" != "null" ]]; then
+    # Need to set global permission so that all user have the read write permission. Need this one for upgrade in Ops req
     chmod 0777 $OLD_SHARE_DIR
     chmod 0777 $OLD_LIB_DIR
 fi
@@ -15,5 +19,5 @@ if [ "${SSL:-0}" = "ON" ]; then
     cp -R /certs/ /tls/
     chmod 0600 /tls/certs/server/*
     chmod 0600 /tls/certs/client/*
-    chown -R $DB_UID:$DB_GID /tls/certs
+    chmod 0600 /tls/certs/exporter/*
 fi
