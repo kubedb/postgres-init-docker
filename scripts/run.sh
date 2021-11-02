551e9b844ae70974e0b54042a0027b02db0664bd
Habibur Rahaman Emon <emon@appscode.com>
2021-11-02 22:59:30 +0600

    Fix: Restart if the Postgres process is killed (#8)
    Signed-off-by: Emon46 <emon@appscode.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
-set -eou pipefail
+
 #going to change this with the check of process id
 rm -f "$PGDATA"/postmaster.pid
 echo "waiting for the role to be decided ..."
 while true; do
     if [[ -e /run_scripts/role/run.sh ]]; then
         echo "running the initial script ..."
-        /run_scripts/role/run.sh && break
+        /run_scripts/role/run.sh
+        echo "removing the initial scripts as server is not running ..."
+        rm -rf /run_scripts/*
     fi
     sleep 1
 done
