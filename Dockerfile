10ca5ba84bf24aabe82f949fc900f2690bf49e32
Emon46 <emon@appscode.com>
2022-09-02 21:24:23 +0600

    #WIP
    Signed-off-by: Emon46 <emon@appscode.com>

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,8 @@ RUN curl -fsSL -o tini https://github.com/kubedb/tini/releases/download/v0.20.0/
 	&& chmod +x tini
 
 
+#RUN curl -fsSL -o wal-g https://github.com/wal-g/wal-g/releases/download/v2.0.1/wal-g-pg-ubuntu-20.04-amd64 \
+#    && chmod +x wal-g
 
 FROM alpine
 
@@ -20,6 +22,7 @@ RUN apk add --no-cache bash
 COPY scripts /tmp/scripts
 COPY init_scripts /init_scripts
 COPY --from=0 /tini /tmp/scripts/tini
+#COPY --from=0 /wal-g /tmp/scripts/wal-g
 COPY role_scripts /tmp/role_scripts
 
 ENTRYPOINT ["/init_scripts/run.sh"]
--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-wal-g backup-fetch /var/pv/data LATEST
-touch /var/pv/data/recovery.signal
 
+wal-g backup-fetch $PGDATA LATEST
+touch $PGDATA/recovery.signal
+
+
+
+# ****************** Recovery config **************************
+#echo "recovery_target_timeline = 'latest'" >>/tmp/postgresql.conf
+touch /tmp/recovery.conf
+echo "restore_command = 'wal-g wal-fetch %f %p'" >>/tmp/recovery.conf
+echo "standby_mode = on" >>/tmp/recovery.conf
+echo "trigger_file = '/run_scripts/tmp/pg-failover-trigger'" >>/tmp/recovery.conf # [ name whose presence ends recovery]
+echo "recovery_target_timeline = 'latest'" >>/tmp/recovery.conf
+mv /tmp/recovery.conf "$PGDATA/recovery.conf"
 
 
 # setup postgresql.conf
 touch /tmp/postgresql.conf
 echo "wal_level = replica" >>/tmp/postgresql.conf
-echo "shared_buffers = $SHARED_BUFFERS" >>/tmp/postgresql.conf
 echo "max_wal_senders = 90" >>/tmp/postgresql.conf # default is 10.  value must be less than max_connections minus superuser_reserved_connections. ref: https://www.postgresql.org/docs/11/runtime-config-replication.html#GUC-MAX-WAL-SENDERS
 
-echo "wal_keep_size = 64" >>/tmp/postgresql.conf
+echo "wal_keep_segments = 64" >>/tmp/postgresql.conf
 
 echo "wal_log_hints = on" >>/tmp/postgresql.conf
 
@@ -18,34 +28,9 @@ echo "wal_log_hints = on" >>/tmp/postgresql.conf
 echo "archive_mode = always" >>/tmp/postgresql.conf
 echo "archive_command = '/bin/true'" >>/tmp/postgresql.conf
 
-if [[ "${SSL:-0}" == "ON" ]]; then
-    echo "ssl =on" >>/tmp/postgresql.conf
-    echo "ssl_cert_file ='/tls/certs/server/server.crt'" >>/tmp/postgresql.conf
-    echo "ssl_key_file ='/tls/certs/server/server.key'" >>/tmp/postgresql.conf
-    echo "ssl_ca_file ='/tls/certs/server/ca.crt'" >>/tmp/postgresql.conf
-fi
 
-if [[ "$CLIENT_AUTH_MODE" == "scram" ]]; then
-    echo "password_encryption = scram-sha-256" >>/tmp/postgresql.conf
-fi
-
-# ****************** Recovery config **************************
-echo "recovery_target_timeline = 'latest'" >>/tmp/postgresql.conf
-# primary_conninfo is used for streaming replication
-if [[ "${SSL:-0}" == "ON" ]]; then
-    if [[ "$CLIENT_AUTH_MODE" == "cert" ]]; then
-        echo "primary_conninfo = 'application_name=$HOSTNAME host=$PRIMARY_HOST user=$POSTGRES_USER password=$POSTGRES_PASSWORD sslmode=$SSL_MODE sslrootcert=/tls/certs/client/ca.crt sslcert=/tls/certs/client/client.crt sslkey=/tls/certs/client/client.key'" >>/tmp/postgresql.conf
-    else
-        echo "primary_conninfo = 'application_name=$HOSTNAME host=$PRIMARY_HOST user=$POSTGRES_USER password=$POSTGRES_PASSWORD sslmode=$SSL_MODE sslrootcert=/tls/certs/client/ca.crt'" >>/tmp/postgresql.conf
-    fi
-else
-    echo "primary_conninfo = 'application_name=$HOSTNAME host=$PRIMARY_HOST user=$POSTGRES_USER password=$POSTGRES_PASSWORD'" >>/tmp/postgresql.conf
-fi
-
-echo "promote_trigger_file = '/run_scripts/tmp/pg-failover-trigger'" >>/tmp/postgresql.conf # [ name whose presence ends recovery]
 
 cat /run_scripts/role/postgresql.conf >>/tmp/postgresql.conf
-echo "restore_command = '/scripts/wal-g wal-fetch %f %p'" >>/tmp/postgresql.conf
 mv /tmp/postgresql.conf "$PGDATA/postgresql.conf"
 
 # setup pg_hba.conf for initial start. this one is just for initialization
@@ -60,9 +45,10 @@ mv /tmp/pg_hba.conf "$PGDATA/pg_hba.conf"
 # start postgres
 pg_ctl -D "$PGDATA" -w start &
 sleep 10
-echo "hey bro i am here................................*****************************......................................."
-while [ -e /var/pv/data/postmaster.pid ] | [ ! -e /var/pv/data/restore.done]; do
-    "restoring..."
+echo "hey bro i am here................................*****************************.......................................  | [[ ! -e /var/pv/data/restore.done]]"
+#  | [[ ! -e /var/pv/data/restore.done]]
+while [[ -e /var/pv/data/postmaster.pid ]]; do
+    echo "restoring..."
     sleep 1
 done
 
