60da2fe76114bb3d06ae409050403b4e2f7344b5
Mehedi Hasan <mehedi.hasan@appscode.com>
2023-08-05 12:15:37 +0600

    update conf
    Signed-off-by: Mehedi Hasan <mehedi.hasan@appscode.com>

--- role_scripts/15/primary/start.sh
+++ role_scripts/15/primary/start.sh
@@ -219,8 +219,8 @@ fi
 if [[ "$STREAMING" == "synchronous" ]]; then
     # setup synchronous streaming replication
     echo "synchronous_commit = remote_write" >>/tmp/postgresql.conf
-    echo "synchronous_standby_names = '*'" >>/tmp/postgresql.conf
+#    echo "synchronous_standby_names = '*'" >>/tmp/postgresql.conf
 fi
-
+echo "include_if_exists = '/etc/config/user.conf'" >>/tmp/postgresql.conf
 # ref: https://superuser.com/a/246841/985093
 cat /tmp/postgresql.conf $PGDATA/postgresql.conf >"/tmp/postgresql.conf.tmp" && mv "/tmp/postgresql.conf.tmp" "$PGDATA/postgresql.conf"
