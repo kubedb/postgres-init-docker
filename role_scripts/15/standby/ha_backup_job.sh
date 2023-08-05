bfe9871177f9959feafd401e3d77992ab2777952
Tamal Saha <tamal@appscode.com>
2023-08-05 00:47:18 -0700

    Update synchronous_standby_names in standby script
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- role_scripts/15/standby/ha_backup_job.sh
+++ role_scripts/15/standby/ha_backup_job.sh
@@ -88,8 +88,24 @@ fi
 
 if [[ "$STREAMING" == "synchronous" ]]; then
     # setup synchronous streaming replication
-    echo "synchronous_commit = remote_write" >>/tmp/postgresql.conf
-    echo "synchronous_standby_names = '*'" >>/tmp/postgresql.conf
+    echo "synchronous_commit = on" >>/tmp/postgresql.conf
+
+    # https://stackoverflow.com/a/44092231/244009
+    self_idx=$(echo $HOSTNAME | grep -Eo '[0-9]+$')
+
+    # https://stackoverflow.com/a/44090126/244009
+    shopt -s extglob
+    sts_prefix=${HOSTNAME%%+([0-9])}
+    # https://unix.stackexchange.com/a/104887/42136
+    sts_prefix=${sts_prefix//-/}
+
+    if [ $self_idx -eq 0 ]; then
+        echo "synchronous_standby_names = 'ANY 1 (${sts_prefix}1, ${sts_prefix}2)'" >>/tmp/postgresql.conf
+    elif [ $self_idx -eq 1 ]; then
+        echo "synchronous_standby_names = 'ANY 1 (${sts_prefix}0, ${sts_prefix}2)'" >>/tmp/postgresql.conf
+    else
+        echo "synchronous_standby_names = 'ANY 1 (${sts_prefix}0, ${sts_prefix}1)'" >>/tmp/postgresql.conf
+    fi
 fi
 
 if [[ "${SSL:-0}" == "ON" ]]; then
