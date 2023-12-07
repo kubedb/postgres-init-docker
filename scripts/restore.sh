dfbdb6f6df4ead35fe41e03028521de7a1f6e8ae
Mehedi Hasan <39819064+heheh13@users.noreply.github.com>
2023-12-07 00:47:57 -0500

    remove raft wal from snapshot restore (#27)
    Signed-off-by: Mehedi Hasan <mehedi.hasan@appscode.com>

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -2,6 +2,8 @@
 
 mkdir -p $PGDATA
 chmod 0700 "$PGDATA"
+# remove raft wal
+rm -rf /var/pv/raftwal && rm -rf /var/pv/raftsnapshot
 
 if [[ "${WALG_BASE_BACKUP_NAME:-0}" != "0" ]]; then
     echo "starting to restore from basebackup $WALG_BASE_BACKUP_NAME ..."
