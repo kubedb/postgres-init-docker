06ba93ab1cc353717866a9acd5d7b90bb30feba6
Tamal Saha <tamal@appscode.com>
2023-12-08 03:06:20 -0800

    Don't restart postgres on SIGTERM
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+STOP=false
+# don't restart postgres on SIGTERM (eg, pod deleted)
+# ref: https://opensource.com/article/20/6/bash-trap
+trap \
+    "{ STOP=true; }" \
+    SIGINT SIGTERM EXIT
+
 #going to change this with the check of process id
 rm -f "$PGDATA"/postmaster.pid
 echo "waiting for the role to be decided ..."
 while true; do
-    if [[ -e /run_scripts/role/run.sh ]]; then
+    if [[ -e /run_scripts/role/run.sh ]] && [[ "$STOP" = false ]]; then
         echo "running the initial script ..."
         if [[ $REMOTE_REPLICA == "true" ]]; then
             /run_scripts/role/remote-replica.sh
