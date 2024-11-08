d33ce365f4ff033c4d17044746765e03ac100fdc
souravbiswassanto <saurov@appscode.com>
2024-11-08 12:46:14 +0600

    Add PITR Replication Strategy Support
    Signed-off-by: souravbiswassanto <saurov@appscode.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-
+RECOVERY_DONE_FILE="/var/pv/"$PITR_UNIX_TIME"_recovery.done"
+PITR_RS=${PITR_REPLICATION_STRATEGY:-none}
 STOP=false
 # don't restart postgres on SIGTERM (eg, pod deleted)
 # ref: https://opensource.com/article/20/6/bash-trap
@@ -7,6 +8,20 @@ trap \
     "{ STOP=true; }" \
     SIGINT SIGTERM EXIT
 
+if [[ "$PITR_RESTORE" == "true" ]]; then
+    while true; do
+      sleep 2
+      echo "Point In Time Recovery In Progress. Waiting for $RECOVERY_DONE_FILE file"
+      if [[ -e "$RECOVERY_DONE_FILE" ]]; then
+        echo "$RECOVERY_DONE_FILE found."
+        break
+      fi
+    done
+fi
+
+if [[ -d $PGDATA && "$PITR_RS" == "fscopy" ]];then
+  chmod 0700 $PGDATA
+fi
 #going to change this with the check of process id
 rm -f "$PGDATA"/postmaster.pid
 echo "waiting for the role to be decided ..."
