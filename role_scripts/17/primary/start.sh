a96fde561b5aea393ae0e1d4bef45b93524429bc
souravbiswassanto <saurov@appscode.com>
2024-12-06 19:10:20 +0600

    temp
    Signed-off-by: souravbiswassanto <saurov@appscode.com>

--- role_scripts/17/primary/start.sh
+++ role_scripts/17/primary/start.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Copyright AppsCode Inc. and Contributors
 #
@@ -14,92 +14,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export PASSWORD
-set -eou pipefail
-
-echo "Running as Remote Replica"
-
-# set password ENV
-export PGPASSWORD=${PRIMARY_PASSWORD:-}
-
-# Waiting for running Postgres
-while true; do
-    echo "Attempting pg_isready on primary"
-
-    if [[ "${SOURCE_SSL:-0}" == "ON" ]]; then
-        pg_isready --host="$PRIMARY_HOST" -d "sslmode=$SOURCE_SSL_MODE sslrootcert=/tls/certs/remote/ca.crt sslcert=/tls/certs/remote/client.crt sslkey=/tls/certs/remote/client.key" --username=$PRIMARY_USER_NAME --timeout=2 &>/dev/null && break
-    else
-        pg_isready --host="$PRIMARY_HOST" --username=$PRIMARY_USER_NAME --timeout=2 &>/dev/null && break
-    fi
-    sleep 2
-done
-
-while true; do
-    echo "Attempting query on primary"
-    if [[ "${SOURCE_SSL:-0}" == "ON" ]]; then
-        psql -h "$PRIMARY_HOST" --username=$PRIMARY_USER_NAME -d "dbname=postgres sslmode=$SOURCE_SSL_MODE sslrootcert=/tls/certs/remote/ca.crt sslcert=/tls/certs/remote/client.crt sslkey=/tls/certs/remote/client.key" --command="select now();" &>/dev/null && break
-    else
-        psql -h "$PRIMARY_HOST" --username=$PRIMARY_USER_NAME -d postgres --no-password --command="select now();" &>/dev/null && break
-    fi
-
-    sleep 2
-done
-
-if [[ ! -e "$PGDATA/PG_VERSION" ]]; then
-    echo "taking base basebackup..."
-    mkdir -p "$PGDATA"
-    rm -rf "$PGDATA"/*
-    chmod 0700 "$PGDATA"
-    if [[ "${SOURCE_SSL:-0}" == "ON" ]]; then
-        pg_basebackup -X fetch --pgdata "$PGDATA" --username=$PRIMARY_USER_NAME --progress --host="$PRIMARY_HOST" -d "sslmode=$SOURCE_SSL_MODE sslrootcert=/tls/certs/remote/ca.crt sslcert=/tls/certs/remote/client.crt sslkey=/tls/certs/remote/client.key"
-    else
-        pg_basebackup -X fetch --no-password --pgdata "$PGDATA" --username=$PRIMARY_USER_NAME --progress --host="$PRIMARY_HOST"
-    fi
-fi
-
 # setup postgresql.conf
 touch /tmp/postgresql.conf
 echo "wal_level = replica" >>/tmp/postgresql.conf
 echo "shared_buffers = $SHARED_BUFFERS" >>/tmp/postgresql.conf
 echo "max_wal_senders = 90" >>/tmp/postgresql.conf # default is 10.  value must be less than max_connections minus superuser_reserved_connections. ref: https://www.postgresql.org/docs/11/runtime-config-replication.html#GUC-MAX-WAL-SENDERS
 
-# echo "wal_keep_size = 64" >>/tmp/postgresql.conf #it was  "wal_keep_segments" in earlier version. changed in version 13
+# echo "wal_keep_size = 2560" >>/tmp/postgresql.conf
 if [ ! -z "${WAL_RETAIN_PARAM:-}" ] && [ ! -z "${WAL_RETAIN_AMOUNT:-}" ]; then
     echo "${WAL_RETAIN_PARAM}=${WAL_RETAIN_AMOUNT}" >>/tmp/postgresql.conf
 else
   echo "wal_keep_size = 2560" >>/tmp/postgresql.conf
 fi
-if [[ "$WAL_LIMIT_POLICY" == "ReplicationSlot" ]]; then
-  CLEAN_HOSTNAME="${HOSTNAME//[^[:alnum:]]/}"
-  echo "primary_slot_name="$CLEAN_HOSTNAME"" >>/tmp/postgresql.conf
-fi
-echo "max_replication_slots = 90" >>/tmp/postgresql.conf
-echo "wal_log_hints = on" >>/tmp/postgresql.conf
 
+echo "wal_log_hints = on" >>/tmp/postgresql.conf
+echo "max_replication_slots = 90" >>/tmp/postgresql.conf
 # we are not doing any archiving by default but it's better to have this config in our postgresql.conf file in case of customization.
 echo "archive_mode = always" >>/tmp/postgresql.conf
-echo "archive_command = '/bin/true'" >>/tmp/postgresql.conf
+if [[ "${WAL_BACKUP_TYPE:-0}" == "WALG" ]]; then
+    echo "archive_command = 'cp %p /var/pv/wal_archive/%f'" >>/tmp/postgresql.conf
+else
+    echo "archive_command = '/bin/true'" >>/tmp/postgresql.conf
+fi
 
 echo "shared_preload_libraries = 'pg_stat_statements'" >>/tmp/postgresql.conf
 
-if [ "$STANDBY" == "hot" ]; then
-    echo "hot_standby = on" >>/tmp/postgresql.conf
-else
-    echo "hot_standby = off" >>/tmp/postgresql.conf
-fi
-
-if [[ "$STREAMING" == "synchronous" ]]; then
-    # setup synchronous streaming replication
-    echo "synchronous_commit = remote_write" >>/tmp/postgresql.conf
-    echo "synchronous_standby_names = '*'" >>/tmp/postgresql.conf
-fi
-
 if [[ "${SSL:-0}" == "ON" ]]; then
-    echo "ssl = on" >>/tmp/postgresql.conf
+    echo "ssl =on" >>/tmp/postgresql.conf
+    echo "ssl_cert_file ='/tls/certs/server/server.crt'" >>/tmp/postgresql.conf
+    echo "ssl_key_file ='/tls/certs/server/server.key'" >>/tmp/postgresql.conf
+    echo "ssl_ca_file ='/tls/certs/server/ca.crt'" >>/tmp/postgresql.conf
+fi
 
-    echo "ssl_cert_file = '/tls/certs/server/server.crt'" >>/tmp/postgresql.conf
-    echo "ssl_key_file = '/tls/certs/server/server.key'" >>/tmp/postgresql.conf
-    echo "ssl_ca_file = '/tls/certs/server/ca.crt'" >>/tmp/postgresql.conf
+if [[ "$CLIENT_AUTH_MODE" == "md5" ]]; then
+    echo "password_encryption = md5" >>/tmp/postgresql.conf
 fi
 
 if [[ "$CLIENT_AUTH_MODE" == "scram" ]]; then
@@ -109,18 +57,85 @@ fi
 # ****************** Recovery config **************************
 echo "recovery_target_timeline = 'latest'" >>/tmp/postgresql.conf
 # primary_conninfo is used for streaming replication
-if [[ "${SOURCE_SSL:-0}" == "ON" ]]; then
-    echo "primary_conninfo = 'application_name=$HOSTNAME host=$PRIMARY_HOST user=$PRIMARY_USER_NAME password=$PRIMARY_PASSWORD sslmode=$SOURCE_SSL_MODE sslrootcert=/tls/certs/remote/ca.crt sslcert=/tls/certs/remote/client.crt sslkey=/tls/certs/remote/client.key'" >>/tmp/postgresql.conf
+if [[ "${SSL:-0}" == "ON" ]]; then
+    if [[ "$CLIENT_AUTH_MODE" == "cert" ]]; then
+        echo "primary_conninfo = 'application_name=$HOSTNAME host=$PRIMARY_HOST user=$POSTGRES_USER password=$POSTGRES_PASSWORD sslmode=$SSL_MODE sslrootcert=/tls/certs/client/ca.crt sslcert=/tls/certs/client/client.crt sslkey=/tls/certs/client/client.key'" >>/tmp/postgresql.conf
+    else
+        echo "primary_conninfo = 'application_name=$HOSTNAME host=$PRIMARY_HOST user=$POSTGRES_USER password=$POSTGRES_PASSWORD sslmode=$SSL_MODE sslrootcert=/tls/certs/client/ca.crt'" >>/tmp/postgresql.conf
+    fi
 else
-    echo "primary_conninfo = 'application_name=$HOSTNAME host=$PRIMARY_HOST user=$PRIMARY_USER_NAME password=$PRIMARY_PASSWORD'" >>/tmp/postgresql.conf
+    echo "primary_conninfo = 'application_name=$HOSTNAME host=$PRIMARY_HOST user=$POSTGRES_USER password=$POSTGRES_PASSWORD'" >>/tmp/postgresql.conf
 fi
-
-echo "promote_trigger_file = '/run_scripts/tmp/pg-failover-trigger'" >>/tmp/postgresql.conf # [ name whose presence ends recovery]
 
 cat /run_scripts/role/postgresql.conf >>/tmp/postgresql.conf
 mv /tmp/postgresql.conf "$PGDATA/postgresql.conf"
 
-touch "$PGDATA/standby.signal"
+# setup pg_hba.conf for initial start. this one is just for initialization
+touch /tmp/pg_hba.conf
+{ echo '#TYPE      DATABASE        USER            ADDRESS                 METHOD'; } >>tmp/pg_hba.conf
+{ echo '# "local" is for Unix domain socket connections only'; } >>tmp/pg_hba.conf
+{ echo 'local      all             all                                     trust'; } >>tmp/pg_hba.conf
+{ echo '# IPv4 local connections:'; } >>tmp/pg_hba.conf
+{ echo 'host         all             all             127.0.0.1/32            trust'; } >>tmp/pg_hba.conf
+mv /tmp/pg_hba.conf "$PGDATA/pg_hba.conf"
+
+# start postgres
+pg_ctl -D "$PGDATA" -w start
+
+export POSTGRES_USER=${POSTGRES_USER:-postgres}
+export POSTGRES_DB=${POSTGRES_DB:-$POSTGRES_USER}
+export POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-postgres}
+
+psql=(psql -v ON_ERROR_STOP=1)
+
+# create database with specified name
+if [ "$POSTGRES_DB" != "postgres" ]; then
+    "${psql[@]}" --username postgres <<-EOSQL
+CREATE DATABASE "$POSTGRES_DB" ;
+EOSQL
+    echo
+fi
+
+if [ "$POSTGRES_USER" = "postgres" ]; then
+    op="ALTER"
+else
+    op="CREATE"
+fi
+
+# alter postgres superuser
+"${psql[@]}" --username postgres <<-EOSQL
+    $op USER "$POSTGRES_USER" WITH SUPERUSER PASSWORD '$POSTGRES_PASSWORD';
+EOSQL
+echo
+
+psql+=(--username "$POSTGRES_USER" --dbname "$POSTGRES_DB")
+echo
+
+# initialize database
+for f in "$INITDB"/*; do
+    case "$f" in
+        *.sh)
+            echo "$0: running $f"
+            . "$f"
+            ;;
+        *.sql)
+            echo "$0: running $f"
+            "${psql[@]}" -f "$f"
+            echo
+            ;;
+        *.sql.gz)
+            echo "$0: running $f"
+            gunzip -c "$f" | "${psql[@]}"
+            echo
+            ;;
+        *) echo "$0: ignoring $f" ;;
+    esac
+    echo
+done
+
+# stop server
+pg_ctl promote
+pg_ctl -D "$PGDATA" -m fast -w stop
 
 # setup pg_hba.conf
 touch /tmp/pg_hba.conf
@@ -145,6 +160,8 @@ if [[ "${SSL:-0}" == "ON" ]]; then
         { echo 'hostssl    replication     postgres        0.0.0.0/0               cert clientcert=verify-full'; } >>tmp/pg_hba.conf
         { echo 'hostssl    all             all             ::/0                    cert clientcert=verify-full'; } >>tmp/pg_hba.conf
         { echo 'hostssl    replication     postgres        ::/0                    cert clientcert=verify-full'; } >>tmp/pg_hba.conf
+        { echo 'hostssl    replication     all             0.0.0.0/0               cert clientcert=verify-full'; } >>tmp/pg_hba.conf
+        { echo 'hostssl    replication     all             ::/0                    cert clientcert=verify-full'; } >>tmp/pg_hba.conf
     elif [[ "$CLIENT_AUTH_MODE" == "scram" ]]; then
         { echo '# IPv4 local connections:'; } >>tmp/pg_hba.conf
         { echo 'hostssl    all             all             127.0.0.1/32            scram-sha-256'; } >>tmp/pg_hba.conf
@@ -159,6 +176,8 @@ if [[ "${SSL:-0}" == "ON" ]]; then
         { echo 'hostssl    replication     postgres        0.0.0.0/0               scram-sha-256'; } >>tmp/pg_hba.conf
         { echo 'hostssl    all             all             ::/0                    scram-sha-256'; } >>tmp/pg_hba.conf
         { echo 'hostssl    replication     postgres        ::/0                    scram-sha-256'; } >>tmp/pg_hba.conf
+        { echo 'hostssl    replication     all             0.0.0.0/0               scram-sha-256'; } >>tmp/pg_hba.conf
+        { echo 'hostssl    replication     all             ::/0                    scram-sha-256'; } >>tmp/pg_hba.conf
     else
         { echo '# IPv4 local connections:'; } >>tmp/pg_hba.conf
         { echo 'hostssl    all             all             127.0.0.1/32            md5'; } >>tmp/pg_hba.conf
@@ -173,6 +192,8 @@ if [[ "${SSL:-0}" == "ON" ]]; then
         { echo 'hostssl    replication     postgres        0.0.0.0/0               md5'; } >>tmp/pg_hba.conf
         { echo 'hostssl    all             all             ::/0                    md5'; } >>tmp/pg_hba.conf
         { echo 'hostssl    replication     postgres        ::/0                    md5'; } >>tmp/pg_hba.conf
+        { echo 'hostssl    replication     all             0.0.0.0/0               md5'; } >>tmp/pg_hba.conf
+        { echo 'hostssl    replication     all             ::/0                    md5'; } >>tmp/pg_hba.conf
     fi
 
 else
@@ -190,6 +211,8 @@ else
         { echo 'host         replication     postgres        0.0.0.0/0               scram-sha-256'; } >>tmp/pg_hba.conf
         { echo 'host         all             all             ::/0                    scram-sha-256'; } >>tmp/pg_hba.conf
         { echo 'host         replication     postgres        ::/0                    scram-sha-256'; } >>tmp/pg_hba.conf
+        { echo 'host         replication     all             0.0.0.0/0               scram-sha-256'; } >>tmp/pg_hba.conf
+        { echo 'host         replication     all             ::/0                    scram-sha-256'; } >>tmp/pg_hba.conf
     else
         { echo '# IPv4 local connections:'; } >>tmp/pg_hba.conf
         { echo 'host         all             all             127.0.0.1/32            trust'; } >>tmp/pg_hba.conf
@@ -204,8 +227,41 @@ else
         { echo 'host         replication     postgres        0.0.0.0/0               md5'; } >>tmp/pg_hba.conf
         { echo 'host         all             all             ::/0                    md5'; } >>tmp/pg_hba.conf
         { echo 'host         replication     postgres        ::/0                    md5'; } >>tmp/pg_hba.conf
+        { echo 'host         replication     all             0.0.0.0/0               md5'; } >>tmp/pg_hba.conf
+        { echo 'host         replication     all             ::/0                    md5'; } >>tmp/pg_hba.conf
     fi
+
 fi
 
 mv /tmp/pg_hba.conf "$PGDATA/pg_hba.conf"
-exec postgres
+
+touch /tmp/postgresql.conf
+if [ "$STANDBY" == "hot" ]; then
+    echo "hot_standby = on" >>/tmp/postgresql.conf
+else
+    echo "hot_standby = off" >>/tmp/postgresql.conf
+fi
+if [[ "$STREAMING" == "synchronous" ]]; then
+    # setup synchronous streaming replication
+    echo "synchronous_commit = remote_write" >>/tmp/postgresql.conf
+
+    # https://stackoverflow.com/a/44092231/244009
+    self_idx=$(echo $HOSTNAME | grep -Eo '[0-9]+$')
+    echo "$self_idx"
+
+    shopt -s extglob
+    sts_prefix=${HOSTNAME%%+([0-9])}
+    names=""
+    for ((i = 0; i < $REPLICAS; i++)); do
+        if [[ $self_idx == $i ]]; then
+            echo "skip $i"
+        else
+            names+="\"$sts_prefix$i\","
+        fi
+    done
+    names=$(echo "$names" | rev | cut -c2- | rev)
+    echo "synchronous_standby_names = 'ANY 1 ("$names")'" >>/tmp/postgresql.conf
+fi
+
+# ref: https://superuser.com/a/246841/985093
+cat /tmp/postgresql.conf $PGDATA/postgresql.conf >"/tmp/postgresql.conf.tmp" && mv "/tmp/postgresql.conf.tmp" "$PGDATA/postgresql.conf"
