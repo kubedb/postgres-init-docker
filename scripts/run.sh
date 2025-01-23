41ec8346fdffea74a5ddfcee3cb156baa0f9ef61
souravbiswassanto <saurov@appscode.com>
2025-01-23 19:19:40 +0600

    Update Data Directory Permission If Set Higher than 0700
    Signed-off-by: souravbiswassanto <saurov@appscode.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -26,6 +26,15 @@ fi
 rm -f "$PGDATA"/postmaster.pid
 echo "waiting for the role to be decided ..."
 while true; do
+    if [[ -d $PGDATA ]];then
+      DIR="$PGDATA"
+      CURRENT_PERMS=$(stat -c "%a" "$DIR")
+      if [ "$CURRENT_PERMS" -gt 700 ]; then
+          echo "Permissions are greater than 0700. Updating to 0700."
+          chmod 0700 "$DIR"
+      fi
+    fi
+
     if [[ -e /run_scripts/role/run.sh ]] && [[ "$STOP" = false ]]; then
         echo "running the initial script ..."
         if [[ $REMOTE_REPLICA == "true" ]]; then
